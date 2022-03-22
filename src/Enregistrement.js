f52b24b8bd49c218307e3504c82fd808f6acbfdc
Anin29 <anin.khan00@gmail.com>
2022-03-22 11:11:37 +0100

    3 files modified, 1 deleted

--- src/Enregistrement.js
+++ src/Enregistrement.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.png"
 import "./enregistrement.css"
 
 class Enregistrement extends Component{
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './MainPage.js';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <MainPage/>,
